f9f417d4b19b5b06f2f1423e16d1b04cfb956c6e
htiwana <himmattiwana@gmail.com>
2020-02-22 13:02:37 -0800

    bar graphs added and some data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,61 @@
-var d3 = require("d3");
+//d3 = require("d3")
 
-d3
-    .select(".target")
-    .style("stroke-width", 20) 
+var svgWidth = 3000, svgHeight = 1000, barPadding = 2, scale=svgHeight/20;
+
+
+d3.csv("../data/simpledat.csv", conversor, accessor)
+
+
+function conversor(d){
+
+    return {
+      area: d.area,
+      pop: parseInt(d.pop.replace(",","")),
+      potatoes: +d.potatoes
+    };
+
+}
+
+function accessor(error,data){
+      if(error){
+        console.log(error);
+      }else{
+        console.log(data);
+
+        var barWidth = svgWidth/data.length;
+
+        var svg = d3.select('svg')
+           .attr("width", svgWidth)
+           .attr("height", svgHeight);
+
+        var groups = svg.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g");
+
+        groups.append("rect")
+            .attr("x", (d,i) => i*(barWidth))
+            .attr("y", d => svgHeight - d.pop/scale)
+            .attr("height", function(d) {
+                return d.pop/scale;
+            })
+            .attr("width", barWidth-barPadding);
+
+        groups.append("text")
+            .text(d => d.area)
+            .attr('x',(d,i) => i*(barWidth))
+            .attr('y', 50)
+            .attr("transform", d => "rotate(0)");
+
+      return data;
+      }
+}
+
+
+
+
+// this is just displaying a static svg i created folloing part one of https://medium.com/@mbostock/command-line-cartography-part-1-897aa8f8ca2c
+d3.select("#vanmap").append("image")
+    .attr("xlink:href","../data/van-albers.svg")
+    .attr("width", 500)
+    .attr("height", 500)
