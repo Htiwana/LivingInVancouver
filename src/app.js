94a39f1334828653e7904f206e21e4a050369861
htiwana <himmattiwana@gmail.com>
2020-02-22 18:08:42 -0800

    reordering elements

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-//d3 = require("d3")
 
-var svgWidth = 3000, svgHeight = 1000, barPadding = 2, scale=svgHeight/20;
+var svgWidth = 400, svgHeight = 300, barPadding = 2, scale=svgHeight;
 
 
 d3.csv("../data/simpledat.csv", conversor, accessor)
-// d3.json("https://gist.githubusercontent.com/drlynb/234c622e2f98da7c61393cbb33573bf8/raw/8e8fa5ae3dc76d94dcca3a50e3dce41b89eb7185/vanstreets.json", map)
 
 
 
@@ -17,12 +15,6 @@ function conversor(d){
     };
 }
 
-// this is just displaying a static svg i created folloing part one of https://medium.com/@mbostock/command-line-cartography-part-1-897aa8f8ca2c
-d3.select("#vanmap").append("image")
-    .attr("xlink:href","../data/van-albers.svg")
-    .attr("width", 500)
-    .attr("height", 500)
-
 
 function splitwork(data,map){
   plotbars(data);
@@ -31,10 +23,10 @@ function splitwork(data,map){
 
 
 function plotbars(data){
-  console.log(data);
   var barWidth = svgWidth/data.length;
 
-  var svg = d3.select('svg')
+  var svg = d3.select('#vanbars')
+     .append("svg")
      .attr("width", svgWidth)
      .attr("height", svgHeight);
 
@@ -45,9 +37,9 @@ function plotbars(data){
 
   groups.append("rect")
       .attr("x", (d,i) => i*(barWidth))
-      .attr("y", d => svgHeight - d.pop/scale)
+      .attr("y", d => svgHeight - ((d.pop -6000)/38000)*svgHeight)
       .attr("height", function(d) {
-          return d.pop/scale;
+          return ((d.pop -6000)/38000)*svgHeight;
       })
       .attr("width", barWidth-barPadding);
 
@@ -63,7 +55,7 @@ function plotmap(world,data){
 
   var width = 900,height = 600;
 
-  var mapsvg = d3.select( "body" )
+  var mapsvg = d3.select( "#vanmap" )
       .append( "svg" )
       .attr( "width", width )
       .attr( "height", height );
@@ -86,8 +78,6 @@ function plotmap(world,data){
 
     function opacity_set(d){
       for ( let i = 0; i < 22; i++){
-        console.log(data[i].area);
-        console.log(d.properties.name)
         if(data[i].area == d.properties.name){
           return "" + ((data[i].pop - 6000)/38000)
         }
