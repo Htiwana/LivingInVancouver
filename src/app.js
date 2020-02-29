0b8a7d6fcb876e2fa64a73b256b2f672482e1b34
htiwana <himmattiwana@gmail.com>
2020-02-28 20:17:26 -0800

    mapped price to second map

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
 var svgWidth = 400, svgHeight = 300, barPadding = 2, scale=svgHeight;
 
 
-d3.csv("../data/simpledat.csv", conversor, accessor)
-d3.csv("../data/realdata.csv", realconversor, accessor)
+//d3.csv("../data/simpledat.csv", parser, accessor)
+d3.csv("../data/realdata.csv", real_parser, accessor)
+d3.csv("../data/realdata.csv", real_parser, priceaccessor)
 
-
-function conversor(d){
+function parser(d){
 
     return {
       area: d.area,
@@ -15,21 +15,60 @@ function conversor(d){
     };
 }
 
-function realconversor(d){
-
+function real_parser(d){
+    //console.log(d);
+    var trait = "AverageValueofDwelling2001";
+    var population2001 = makeNumber(d.TotalPop2001);
+    var price2001 = makeNumber(d[trait]);
     return {
-      // area: d.area,
-      // pop: parseInt(d.pop.replace(",","")),
-      // potatoes: +d.potatoes
+      area: d.area,
+      pop2001: population2001,
+      price2001: price2001
     };
 }
 
-
-function splitwork(data,map){
-  plotbars(data);
-  plotmap(map,data);
+function makeNumber(d){
+  d = d.replace(",","");
+  d = d.replace("$","");
+  return parseInt(d);
 }
 
+function accessor(error,data){
+      if(error){
+        console.log(error);
+      }else{
+        d3.json("../data/van.json", map)
+        function map(err, world)
+        {
+          if(err){
+            console.log(err)
+          }else{
+            splitwork(data,world);
+          }
+        }
+    }
+}
+
+function priceaccessor(error,data){
+      if(error){
+        console.log(error);
+      }else{
+        d3.json("../data/van.json", map)
+        function map(err, world)
+        {
+          if(err){
+            console.log(err)
+          }else{
+            plotpricemap(world,data);
+          }
+        }
+    }
+}
+
+function splitwork(data,map){
+  //plotbars(data);
+  plotmap(map,data);
+}
 
 function plotbars(data){
   var barWidth = svgWidth/data.length;
@@ -61,7 +100,7 @@ function plotbars(data){
 
 
 function plotmap(world,data){
-  console.log(world)
+  //console.log(world)
   var width = 900,height = 600;
 
   var mapsvg = d3.select( "#vanmap" )
@@ -86,7 +125,7 @@ function plotmap(world,data){
     .attr( "d", geoPath )
     .attr("class",d=>d.properties.name)
     .attr('fill', "#0f4c75")
-    .attr('fill-opacity',opacity_set)
+    .attr('fill-opacity',pop_opacity)
 
 	// Tooltip on mouse over area
 	.on("mousemove", function(d)
@@ -110,24 +149,92 @@ function plotmap(world,data){
 	    selectedValue = this.value
 	})
 
-    function opacity_set(d){
+    function pop_opacity(d){
       for ( let i = 0; i < 22; i++){
         if(data[i].area == d.properties.name){
-          return "" + (((data[i].pop)/38000))
+          //console.log(data[i].pop2001);
+          return "" + (((data[i].pop2001)/38000))
         }
       }
     }
+
+    function price_opacity(d){
+      for ( let i = 0; i < 22; i++){
+        if(data[i].area == d.properties.name){
+          console.log(data[i].price2001);
+          return "" + (((data[i].price2001)/880000))
+        }
+      }
+    }
+
+
 }
 
+function plotpricemap(world,data){
+  //console.log(world)
+  var width = 900,height = 600;
 
-function accessor(error,data){
-      if(error){
-        console.log(error);
-      }else{
-        d3.json("../data/van.json", map)
-        function map(err, world)
-        {
-          splitwork(data,world);
+  var mapsvg = d3.select( "body" )
+    .append( "svg" )
+    .attr( "width", width )
+    .attr( "height", height );
+
+  var tooltip = d3.select("body").append('div')
+    .attr("class", "hidden tooltip");
+
+  // Projection
+  var projection = d3.geoMercator().fitExtent([[10, 10], [800 - 10, 600 - 10]], world)
+
+  var geoPath = d3.geoPath()
+    .projection(projection);
+
+  var areas = mapsvg.append("g")
+    .selectAll("path")
+    .data(world.features)
+    .enter()
+    .append("path")
+    .attr( "d", geoPath )
+    .attr("class",d=>d.properties.name)
+    .attr('fill', "#0f4c75")
+    .attr('fill-opacity',price_opacity)
+
+	// Tooltip on mouse over area
+	.on("mousemove", function(d)
+	{
+		var mouse = d3.mouse(mapsvg.node()).map(function(d) {
+			return parseInt(d);
+		})
+
+		tooltip.classed("hidden", false)
+			.attr("style", "left: " + (mouse[0] + 15) + "px; top:" + (mouse[1] + 650) + "px")// EXTREMELY HACKY Y COORD FIX
+            .text(d.properties.name);
+	})
+	.on("mouseout", function()
+	{
+		tooltip.classed("hidden", true);
+	});
+
+	// Listen to the slider
+	d3.select("#mySlider").on("change", function(d){
+		// Recover slide value
+	    selectedValue = this.value
+	})
+
+    function pop_opacity(d){
+      for ( let i = 0; i < 22; i++){
+        if(data[i].area == d.properties.name){
+          //console.log(data[i].pop2001);
+          return "" + (((data[i].pop2001)/38000))
         }
+      }
+    }
+
+    function price_opacity(d){
+      for ( let i = 0; i < 22; i++){
+        if(data[i].area == d.properties.name){
+          console.log(data[i].price2001);
+          return "" + (((data[i].price2001)/880000))
+        }
+      }
     }
 }
