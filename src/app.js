ec44146120ea651b8321069041836ecdfdbaf97e
Allan <allanl_95@hotmail.com>
2020-04-05 14:23:43 -0700

    legend

--- src/app.js
+++ src/app.js
@@ -46,6 +46,7 @@ function accessor(error,data){
             dataglobal = data;
             worldglobal = world;
             plotmap();
+			legend(300000);
           }
         }
     }
@@ -184,12 +185,14 @@ function updateMap(updatOption)
 {
  if (updatOption.localeCompare("Market Value") == 0)
  {
-   dimensionglobal = "price"
-	 plotmap();
+	dimensionglobal = "price"
+	plotmap();
+	legend(max)
  }
  else {
-   dimensionglobal = "pop"
-   plotmap();
+	dimensionglobal = "pop"
+	plotmap();
+	legend(300000)
  }
 }
 
@@ -222,4 +225,76 @@ function changeYear(year)
 		yearglobal = "2016";
 	}
 	plotmap();
+}
+
+// Legend
+function legend(max) {
+	var colorScale = d3.scaleLinear()
+		// .domain([0,	10,	15,	20, 25, 100])
+		.domain([0, max])
+		.range(['#E28672','#A9FCAA']);
+
+	  // append a defs (for definition) element to your SVG
+	var svgLegend = d3.select('#legend').append('svg')
+		.attr("width",600);
+	var defs = svgLegend.append('defs');
+
+		// append a linearGradient element to the defs and give it a unique id
+	var linearGradient = defs.append('linearGradient')
+			.attr('id', 'linear-gradient');
+
+	// horizontal gradient
+	linearGradient
+	  .attr("x1", "0%")
+	  .attr("y1", "0%")
+	  .attr("x2", "100%")
+	  .attr("y2", "0%");
+
+	// append multiple color stops by using D3's data/enter step
+	linearGradient.selectAll("stop")
+	  .data([
+		{offset: "0%", color: "whitesmoke"},
+		// {offset: "10%", color: "#EC93AB"},
+		// {offset: "15%", color: "#CEB1DE"},
+		// {offset: "20%", color: "#95D3F0"},
+		// {offset: "25%", color: "#77EDD9"},
+		{offset: "100%", color: "#0f4c75"}
+	  ])
+	  .enter().append("stop")
+	  .attr("offset", function(d) { 
+		return d.offset; 
+	  })
+	  .attr("stop-color", function(d) { 
+		return d.color; 
+	  });
+
+	// append title
+	svgLegend.append("text")
+	  .attr("class", "legendTitle")
+	  .attr("x", 10)
+	  .attr("y", 20)
+	  .style("text-anchor", "left")
+	  .text("Legend");
+
+	// draw the rectangle and fill with gradient
+	svgLegend.append("rect")
+	  .attr("x", 10)
+	  .attr("y", 30)
+	  .attr("width", 400)
+	  .attr("height", 15)
+	  .style("fill", "url(#linear-gradient)");
+
+	//create tick marks
+	var xLeg = d3.scaleLinear()
+	  .domain([0, max])
+	  .range([10, 409]); //width of the bar
+
+	var axisLegend = d3.axisBottom(xLeg)
+	  .tickValues(colorScale.domain())
+
+	svgLegend
+	  .attr("class", "axis")
+	  .append("g")
+	  .attr("transform", "translate(0, 40)")
+	  .call(axisLegend);
 }
